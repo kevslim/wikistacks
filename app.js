434d5953426387d2c4a0e8a4b0cc3e54624a7113
jgierlach <jegierlach@gmail.com>
2018-06-19 10:14:45 -0500

    added error page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const path = require("path");
+const error = require("./views/error");
 
 app.use(morgan("dev")); //logging middleware
 app.use(express.static(path.join(__dirname, "./public"))); //serving up static files (e.g. css files)
@@ -11,9 +12,16 @@ app.use(bodyParser.json());
 
 app.use("/wiki", require("./routes/wiki"));
 app.use("/users", require("./routes/users"));
+app.use("/error", require("./views/error"));
 
 app.get('/', function (req, res) {
    res.redirect('/wiki/');
 });
+
+//error handling
+app.use((err, req, res, next) => {
+    //console.log(err)
+    res.status(404).send(error('You have an error', err));
+})
 
 module.exports = app;
